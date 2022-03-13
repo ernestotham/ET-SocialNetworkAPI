28a19c3b9e3560f294ef8c8256505b93a9d13703
Ernesto Tham <ernesto.tham@kbslp.com>
2022-03-13 01:27:12 -0500

    initial commint for socialNet API

---
+++ controllers/models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create Thoughts model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    userName: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'userSchema'
+    }
+    ],
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'reactionsSchema'
+    }
+    ],
+    
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Tought = model('thought', thoughtSchema);
+
+module.exports = Tought;
---
+++ controllers/models/User.js
@@ -0,0 +1,53 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique:true,
+      trim: true,
+            
+    },
+    email: {
+      type: String,
+      required: true,
+      unique:true,
+      trim: true,
+      match: `/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/`,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought'
+      }
+
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'userSchema'
+    }
+    ],
+    
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
